a74039a1293a48e312c6750a31c4b8c9d13543d6
Evil0ctal <20760448+Evil0ctal@users.noreply.github.com>
2021-06-23 15:06:10 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import re
+
+def check(email):
+    regex = '\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
+    if (re.search(regex, email)):
+        return ("Valid Email")
+    else:
+        return ("Invalid Email")
+
+def registration(email):
+    if check(email) == "Valid Email":
+        try:
+            registration_url = "https://calibear.bebooking.enes.tech/user/"
+            registration_payload = {"login": email}
+            registration_response = requests.post(registration_url, data=json.dumps(registration_payload),headers=headers).text
+            response_id = json.loads(registration_response)
+            user_id = int(response_id['id'])
+            return user_id
+        except:
+            print("Something wrong with registration,please contact our staff.")
+    else:
+        print("Please enter an valid email address.")
+        return -1
+
+def activation(activation_code):
+    try:
+        activation_payload = {"activation_code": str(activation_code)}
+        activation_url = "https://calibear.bebooking.enes.tech/user/" + str(registration(email)) + "/activate/"
+        #print(activation_url)
+        activation_response = requests.post(activation_url, data=json.dumps(activation_payload), headers=headers).text
+        #print(activation_response)
+        activation_result = json.loads(activation_response)
+        if "detail" in activation_result:
+            print(str(activation_result['detail']))
+        else:
+            print("Thanks for register at Calibear!\nYour account information and password had been send to your email!")
+    except:
+        print("Something wrong with activation,please contact our staff.")
+
+while True:
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Referer": "https://calibear.booking.enes.tech/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36",
+    }
+    try:
+        email = str(input("Please Input your Email: "))
+        if registration(email) != -1:
+            print("The email you input is: " + email + "\nUser ID: " + str(registration(email)))
+            activation_code = int(input("An activation code had been sent to your Email!\nPlease input the activation code here: "))
+            activation(activation_code)
+    except:
+        print("Something wrong this program,please contact our staff.")
